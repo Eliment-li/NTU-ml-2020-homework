e5a79a6f2f79b5e3c0339b52d2ceb3e259389815
李天 <49738633+Eliment-li@users.noreply.github.com>
2021-02-23 21:11:16 +0800

    hw1 temp

--- hw1/Regression/LinearRegression.py
+++ hw1/Regression/LinearRegression.py
@@ -1,5 +1,4 @@
 # 由前 9 個小時的 18 個 features (包含 PM2.5)預測的 10 個小時的 PM2.5。文档
-
 import numpy as np
 import pandas as pd
 
@@ -11,7 +10,6 @@ def LoadData(path):
     data[data == 'NR'] = 0
     raw_data = data.to_numpy()
     return raw_data
-
 
 # Preprocessing
 
@@ -33,5 +31,21 @@ if __name__ == '__main__':
         sample = np.empty([18, 480])
         for day in range(20):
             sample[:, day * 24: (day + 1) * 24] = raw_data[18 * (20 * month + day): 18 * (20 * month + day + 1), :]
-
         month_data[month] = sample
+    x = np.empty([12 * 471, 18 * 9], dtype=float)
+    y = np.empty([12 * 471, 1], dtype=float)
+
+    for month in range(12):
+
+        for day in range(20):
+
+            for hour in range(24):
+
+                if day == 19 and hour > 14:
+                    continue
+                x[month * 471 + day * 24 + hour, :] = month_data[month][:,
+                                                      day * 24 + hour: day * 24 + hour + 9].reshape(1,
+                                                                                                    -1)  # vector dim:18*9 (9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9)
+                y[month * 471 + day * 24 + hour, 0] = month_data[month][9, day * 24 + hour + 9]  # value
+    print(x)
+    # print(y)
